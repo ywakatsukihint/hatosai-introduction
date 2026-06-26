67243746a2d7672a065dc67a9d90d9c41d730868
ywakatsukihint <y.wakatsuki04@gmail.com>
2026-06-26 21:11:20 +0900

    Add files via upload

---
+++ data/map.js
@@ -0,0 +1,121 @@
+const mapData = {
+  "bldg-1": {
+    "name": "1棟",
+    "floors": {
+      "2F": [
+        { "title": "屋代新聞班記録帖の配布", "organizer": "新聞班", "location": "122教室", "fee": "無料", "link": "club_activity.html" },
+        { "title": "数学アルゴリズム班 大展示会", "organizer": "数学アルゴリズム班", "location": "122教室", "fee": "無料", "link": "club_activity.html" },
+        { "title": "班誌配布", "organizer": "文学班", "location": "122教室", "fee": "無料", "link": "club_activity.html" },
+        { "title": "プレイルーム", "organizer": "3-1", "location": "123教室", "fee": "無料", "link": "exhibit.html" }
+      ],
+      "3F": [
+        { "title": "カラダ探し〜呪われた教室〜", "organizer": "3-2", "location": "131教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "Harry Potter    Shadow of Hogwarts", "organizer": "3-3", "location": "132教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "SUPER MARIO 3-4", "organizer": "3-4", "location": "133教室", "fee": "無料", "link": "exhibit.html" }
+      ],
+      "4F": [
+        { "title": "ステンドグラスアート", "organizer": "全校制作係", "location": "多目的室", "fee": "無料", "link": "events.html" },
+        { "title": "縁日", "organizer": "中1年・高1年", "location": "多目的室", "fee": "無料", "link": "exhibit.html"},
+        { "title": "ジュラシック・パーク~生還せよ最後の生存者~", "organizer": "3-5", "location": "141教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "カジノ", "organizer": "3-6", "location": "142教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "佐藤寺院", "organizer": "3-7", "location": "143教室", "fee": "無料", "link": "exhibit.html" }
+      ]
+    }
+  },
+  
+  "bldg-2": {
+    "name": "2棟",
+    "floors": {
+      "1F": [
+        { "title": "Addiction", "organizer": "放送同好会", "location": "214教室", "fee": "無料", "link": "club_activity.html" },
+        { "title": "トムとジェリー〜ジェリーになってチーズを持ち帰れ！〜", "organizer": "2-1", "location": "213教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "名探偵コナン灯籠の矛盾", "organizer": "2-2", "location": "212教室", "fee": "無料", "link": "exhibit.html" }
+      ],
+      "2F": [
+        { "title": "3番出口", "organizer": "2-3", "location": "224教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "ポケットモンスター -トロッコと幻のポケモン-", "organizer": "2-4", "location": "223教室", "fee": "無料", "link": "exhibit.html" }
+      ],
+      "3F": [
+        { "title": "トイストーリーマニア", "organizer": "2-5", "location": "234教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "235（存在しない）教室からの脱出", "organizer": "2-6", "location": "233教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "屋代荘", "organizer": "2-7", "location": "232教室", "fee": "無料", "link": "exhibit.html" }
+      ]
+    }
+  },
+  
+  "bldg-3": {
+    "name": "3棟",
+    "floors": {
+      "1F": [
+        { "title": "チョコバナナ", "organizer": "剣道班", "location": "被覆室", "fee": "有料", "link": "club_activity.html" },
+        { "title": "鳩祭茶会", "organizer": "茶道班", "location": "書道室", "fee": "無料", "link": "club_activity.html" },
+        { "title": "", "organizer": "", "location": "", "fee": "", "link": "" }
+      ],
+      "2F": [
+        { "title": "図書館展", "organizer": "図書館展係", "location": "理科講義室", "fee": "有料", "link": "events.html" },
+        { "title": "交流対局場", "organizer": "将棋班", "location": "地学室", "fee": "無料", "link": "club_activity.html" }
+      ],
+      "3F": [
+        { "title": "理数科展", "organizer": "理数科展係", "location": "理科実験室", "fee": "無料", "link": "events.html" },
+        { "title": "プラネタリウム", "organizer": "天文班", "location": "物理教室", "fee": "無料", "link": "club_activity.html" },
+        { "title": "理化班展", "organizer": "理化班", "location": "化学教室", "fee": "無料", "link": "club_activity.html" }
+      ],
+      "4F": [
+        { "title": "英語ディベート", "organizer": "英語班", "location": "LL教室", "fee": "無料", "link": "club_activity.html" },
+        { "title": "鳩祭公演", "organizer": "演劇班", "location": "342教室", "fee": "無料", "link": "club_activity.html" }
+      ]
+    }
+  },
+  
+  "bldg-jhs": {
+    "name": "中学棟",
+    "floors": {
+      "1F": [
+        { "title": "みんなの鳩会議", "organizer": "MHK係", "location": "技術室", "fee": "有料", "link": "events.html" }
+      ],
+      "2F": [
+        { "title": "ズートピア", "organizer": "2-A", "location": "2-A教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "七番目の遺言 ～廃校の七不思議～", "organizer": "2-B", "location": "2-B教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "R.E.P.O.", "organizer": "3-A", "location": "3-A教室", "fee": "無料", "link": "exhibit.html" },
+        { "title": "No, I'm A not Human.", "organizer": "3-B", "location": "3-B教室", "fee": "無料", "link": "exhibit.html" }
+      ]
+    }
+  },
+
+  "bldg-1-2": {
+    "name": "中庭",
+    "events": [
+        { "title": "かき氷", "organizer": "女子バレーボール班", "location": "", "fee": "有料", "link": "club_activity.html" },
+        { "title": "からあげ", "organizer": "卓球班", "location": "", "fee": "有料", "link": "club_activity.html" },
+        { "title": "キンキンに冷えた飲み物", "organizer": "男子バレーボール班", "location": "", "fee": "有料", "link": "club_activity.html" },
+        { "title": "中庭企画", "organizer": "中庭企画係", "location": "", "fee": "無料", "link": "events.html" }
+    ]
+  },
+
+  "bldg-2-3": {
+    "name": "2棟と3棟の間",
+    "events": [
+        { "title": "模擬店 フランクフルト", "organizer": "3-3男子バスケットボール班", "location": "", "fee": "有料", "link": "club_activity.html" },
+        { "title": "フライドポテト", "organizer": "バドミントン班", "location": "", "fee": "有料", "link": "club_activity.html" },
+        { "title": "サーティワンアイスクリーム", "organizer": "弓道班", "location": "", "fee": "有料", "link": "club_activity.html" },
+        { "title": "クレープ", "organizer": "山岳班", "location": "", "fee": "有料", "link": "club_activity.html" },
+        { "title": "焼きそば", "organizer": "女子バスケットボール班", "location": "", "fee": "有料", "link": "club_activity.html" }
+    ]
+  },
+
+  "bldg-gym1": {
+    "name": "第一体育館"
+  },
+  
+  "bldg-gym2": {
+    "name": "第二体育館",
+    "events": [
+        { "title": "ギター・マンドリン班", "organizer": "ギター・マンドリン班", "location": "", "fee": "無料", "link": "club_activity.html" },
+        { "title": "ダンス発表", "organizer": "ダンス班", "location": "", "fee": "無料", "link": "club_activity.html" },
+        { "title": "軽音班ライブ物販", "organizer": "軽音班", "location": "", "fee": "有料", "link": "club_activity.html" },
+        { "title": "文化班発表", "organizer": "合唱班", "location": "", "fee": "無料", "link": "club_activity.html" },
+        { "title": "書道パフォーマンス", "organizer": "書道班", "location": "", "fee": "無料", "link": "club_activity.html" },
+        { "title": "中夜祭 Special Stage", "organizer": "中夜祭係", "location": "", "fee": "無料", "link": "events.html" }
+    ]
+  }
+}
---
+++ data/timetable.js
@@ -0,0 +1,27 @@
+    const eventsData = {
+      day1: [
+        { title: "中学合唱班発表", org: "中学合唱班", location: "gym2", start: "09:30", end: "10:30", type: "music", url: "club_activity.html" },
+        { title: "弦楽班発表", org: "弦楽班", location: "gym2", start: "10:30", end: "11:30", type: "music", url: "club_activity.html" },
+        { title: "軽音班発表", org: "軽音班", location: "gym2", start: "11:30", end: "14:30", type: "music", url: "club_activity.html" },
+        { title: "ダンス班発表", org: "ダンス班", location: "gym2", start: "14:30", end: "15:30", type: "music", url: "club_activity.html" },
+        { title: "みんなの鳩会議", org: "MHK係", location: "jhs", start: "9:30", end: "13:00", type: "special", url: "events.html" },
+        { title: "理数科展", org: "理数科展係", location: "bldg3-3", start: "13:00", end: "15:00", type: "special", url: "events.html" },
+        { title: "演劇班 鳩祭公演", org: "演劇班", location: "bldg3-4", start: "13:00", end: "14:00", type: "stage", url: "club_activity.html" },
+        { title: "茶道班 展示", org: "茶道班", location: "bldg3-s", start: "10:30", end: "12:00", type: "exhibit", url: "club_activity.html" },
+        { title: "茶道班 展示", org: "茶道班", location: "bldg3-s", start: "12:30", end: "14:30", type: "exhibit", url: "club_activity.html" },
+        { title: "月見亭", org: "月見亭係", location: "bldg3-1", start: "10:30", end: "13:30", type: "special", url: "club_activity.html" },
+        { title: "ハトヤ喫茶", org: "ハトヤ喫茶", location: "trcp", start: "10:00", end: "15:00", type: "special", url: "events.html" }
+        
+      ],
+      day2: [
+        { title: "中高吹奏楽班発表", org: "吹奏楽班", location: "gym2", start: "09:45", end: "11:00", type: "music", url: "club_activity.html" },
+        { title: "ギターマンドリン班", org: "ギターマンドリン班", location: "gym2", start: "11:30", end: "12:30", type: "music", url: "club_activity.html" },
+        { title: "合唱班発表", org: "合唱班", location: "gym2", start: "12:30", end: "13:30", type: "music", url: "club_activity.html" },
+        { title: "書道班発表", org: "書道班", location: "gym2", start: "13:30", end: "14:30", type: "stage", url: "club_activity.html" },
+        { title: "中学科学班発表", org: "中学科学班", location: "bldg3-3", start: "9:30", end: "10:45", type: "special", url: "club_activity.html" },
+        { title: "演劇班 鳩祭公演", org: "演劇班", location: "bldg3-4", start: "10:45", end: "12:00", type: "stage", url: "club_activity.html" },
+        { title: "茶道班 展示", org: "茶道班", location: "bldg3-s", start: "10:00", end: "13:00", type: "exhibit", url: "club_activity.html" },
+        { title: "月見亭", org: "月見亭係", location: "bldg3-1", start: "10:30", end: "13:30", type: "special", url: "club_activity.html" },
+        { title: "ハトヤ喫茶", org: "ハトヤ喫茶", location: "trcp", start: "10:00", end: "15:00", type: "special", url: "events.html" }
+      ]
+    };
